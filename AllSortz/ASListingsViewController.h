341b187bff763436ad1bcb7f544c5dc2072a28ea
Lawrence Velázquez <larryv@alum.mit.edu>
2012-07-19 08:59:17 -0400

    Create ASListingsViewController for listings tab.
    We need this subclassed UITableViewController to set up the custom views
    on the nav bar.

---
+++ AllSortz/ASListingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ASListingsViewController.h
+//  AllSortz
+//
+//  Created by Lawrence Velázquez on 7/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ASListingsViewController : UITableViewController
+
+@end
