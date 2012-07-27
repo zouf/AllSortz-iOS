2c8e6724fb0712ef399bbfda59a893161655d938
Connie Wan <connie@allsortz.com>
2012-07-27 12:02:07 -0400

    Create new top-level tab for photo gallery dev.
    Add new tab bar item, new nav controller, and new custom table view
    controller.

---
+++ AllSortz/ASPhotoGridControllerViewController.h
@@ -0,0 +1,13 @@
+//
+//  ASPhotoGridControllerViewController.h
+//  AllSortz
+//
+//  Created by Connie Wan on 7/27/12.
+//  Copyright (c) 2012 AllSortz, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ASPhotoGridControllerViewController : UITableViewController
+
+@end
