39d67fe0318ea456dc7a228d187e13ce214ab19a
Lawrence Velázquez <larryv@allsortz.com>
2012-07-30 15:31:41 -0400

    Create controller for business list class.
    ASBusinessListDataController acts as the proxy between the local model
    objects and the API. It maintains a strong reference to a single
    instance of ASBusinessList. When it receives an updateData: message, it
    queries the API and begins downloading the response asynchronously. When
    the response is fully downloaded, it is parsed and used to initialize
    a new copy of ASBusinessList, which replaces the old one.

---
+++ AllSortz/ASBusinessListDataController.h
@@ -0,0 +1,24 @@
+//
+//  ASBusinessDataController.h
+//  AllSortz
+//
+//  Created by Lawrence Velázquez on 7/28/12.
+//  Copyright (c) 2012 AllSortz, Inc. All rights reserved.
+//
+
+#import "ASBusinessList.h"
+
+
+@interface ASBusinessListDataController : NSObject <NSURLConnectionDataDelegate>
+
+@property (strong, readonly) ASBusinessList *businessList;
+
+- (BOOL)updateData;
+
+// NSURLConnectionDataDelegate
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response;
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data;
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error;
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection;
+
+@end
---
+++ AllSortz/ASBusinessListDataController.m
@@ -0,0 +1,70 @@
+//
+//  ASBusinessDataController.m
+//  AllSortz
+//
+//  Created by Lawrence Velázquez on 7/28/12.
+//  Copyright (c) 2012 AllSortz, Inc. All rights reserved.
+//
+
+#import "ASBusinessListDataController.h"
+
+
+@interface ASBusinessListDataController ()
+
+@property (strong, readwrite) ASBusinessList *businessList;
+@property (strong) NSMutableData *receivedData;
+
+@end
+
+
+@implementation ASBusinessListDataController
+
+- (BOOL)updateData
+{
+    static NSString *address = @"http://allsortz.com/ios/businesses/?uname=zouf";
+    NSURL *url = [NSURL URLWithString:address];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+
+    NSURLConnection *connection = [NSURLConnection connectionWithRequest:request delegate:self];
+    if (!connection) {
+        // TODO: Some proper failure handling maybe
+        return NO;
+    }
+    self.receivedData = [NSMutableData data];
+
+    return YES;
+}
+
+/*
+    NSURLConnectionDataDelegate methods
+*/
+
+// TODO: Handle server problems and non-200 responses
+
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response
+{
+    // Reset data store on new requests
+    [self.receivedData setLength:0];
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data
+{
+    [self.receivedData appendData:data];
+}
+
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error
+{
+    // TODO: Actual error handling
+    self.receivedData = nil;
+}
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection
+{
+    NSMutableDictionary *JSONresponse = [NSJSONSerialization JSONObjectWithData:self.receivedData
+                                                                        options:0
+                                                                          error:NULL];
+    self.businessList = [[ASBusinessList alloc] initWithJSONObject:JSONresponse];
+    self.receivedData = nil;
+}
+
+@end
