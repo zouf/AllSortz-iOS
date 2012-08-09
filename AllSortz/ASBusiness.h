b1143d5a4f2462855393ef01155e8e853f2b059c
Lawrence Velázquez <larryv@allsortz.com>
2012-08-09 17:12:18 -0400

    Create model class for business details.

---
+++ AllSortz/ASBusiness.h
@@ -0,0 +1,11 @@
+//
+//  ASBusiness.h
+//  AllSortz
+//
+//  Created by Lawrence Velázquez on 8/9/12.
+//  Copyright (c) 2012 AllSortz, Inc. All rights reserved.
+//
+
+@interface ASBusiness : NSObject
+
+@end
---
+++ AllSortz/ASBusiness.m
@@ -0,0 +1,14 @@
+//
+//  ASBusiness.m
+//  AllSortz
+//
+//  Created by Lawrence Velázquez on 8/9/12.
+//  Copyright (c) 2012 AllSortz, Inc. All rights reserved.
+//
+
+#import "ASBusiness.h"
+
+
+@implementation ASBusiness
+
+@end
