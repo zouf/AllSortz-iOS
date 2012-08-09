4cbda5ea0a503a15a8151e7d8789de5ce0376e93
Lawrence Velázquez <larryv@allsortz.com>
2012-08-09 15:39:46 -0400

    Create VC for business details view.

---
+++ AllSortz/ASBusinessDetailsViewController.h
@@ -0,0 +1,11 @@
+//
+//  ASBusinessDetailsViewController.h
+//  AllSortz
+//
+//  Created by Lawrence Velázquez on 8/9/12.
+//  Copyright (c) 2012 AllSortz, Inc. All rights reserved.
+//
+
+@interface ASBusinessDetailsViewController : UITableViewController
+
+@end
---
+++ AllSortz/ASBusinessDetailsViewController.m
@@ -0,0 +1,14 @@
+//
+//  ASBusinessDetailsViewController.m
+//  AllSortz
+//
+//  Created by Lawrence Velázquez on 8/9/12.
+//  Copyright (c) 2012 AllSortz, Inc. All rights reserved.
+//
+
+#import "ASBusinessDetailsViewController.h"
+
+
+@implementation ASBusinessDetailsViewController
+
+@end
