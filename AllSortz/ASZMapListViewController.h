0df8eae7910894148d17126dd5232fc649ec27f6
Lawrence Velázquez <larryv@allsortz.com>
2012-10-03 15:25:07 -0400

    Create files for new Map/List ("Explore") view.
    Let's experiment with using a nib for UI.

---
+++ AllSortz/ASZMapListViewController.h
@@ -0,0 +1,11 @@
+//
+//  ASZMapListViewController.h
+//  AllSortz
+//
+//  Created by Lawrence Velázquez on 10/3/12.
+//  Copyright (c) 2012 AllSortz, Inc. All rights reserved.
+//
+
+@interface ASZMapListViewController : UIViewController
+
+@end
---
+++ AllSortz/ASZMapListViewController.m
@@ -0,0 +1,21 @@
+//
+//  ASZMapListViewController.m
+//  AllSortz
+//
+//  Created by Lawrence Velázquez on 10/3/12.
+//  Copyright (c) 2012 AllSortz, Inc. All rights reserved.
+//
+
+#import "ASZMapListViewController.h"
+
+
+@implementation ASZMapListViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    if (!(self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]))
+        return nil;
+    return self;
+}
+
+@end
